a2b676bb4e529c21f1f27d1aea1b4a0d029346ff
Yvonneuv <ylicenga5@gmail>
2023-02-17 15:35:54 -0800

    Tssk3 alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Print alphabet in lowercase and then upper case followed by new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	c = 'A';
+
+	while (c <= 'Z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
