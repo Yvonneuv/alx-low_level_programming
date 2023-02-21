36412bce08e357d454d19ea2886a8201d62fc71b
Yvonneuv <ylicenga5@gmail>
2023-02-21 15:47:53 -0800

    Task2 10 alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "mai.h"
+/**
+ * print_alphabet_x10 - function that prints 10 times the alphabet
+ * in lowercase, followed by a new line.
+ */
+void print_alphabet_x10(void)
+{
+	int ten;
+	char la;
+
+	for (ten = 0; ten <= 9; ten++)
+	{
+		for (la = 'a'; la <= 'z'; la++)
+			_putchar(la);
+		_putchar('\n');
+	}
+}
