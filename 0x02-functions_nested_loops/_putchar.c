80f5fc18a6966e9e9e7ab623d9b62ce2c0bfcedc
Yvonneuv <ylicenga5@gmail>
2023-02-21 15:02:30 -0800

    header file

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * main -  writes character c to stdout
+ * @c: the character to print
+ *
+ * Return: 1 success
+ */
+{
+	return (write(1, &c, 1));
+}
