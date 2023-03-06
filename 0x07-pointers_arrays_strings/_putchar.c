c3eed0d5101a3ca7b44784aefcc78ed15cc44460
Yvonneuv <ylicenga5@gmail>
2023-03-06 14:51:58 -0800

    mainh file

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * main -  writes character c to stdout
+ * @c: the character to print
+ *
+ * Return: 1 success
+ */
+
+int _putchar(char c)
+
+{
+        return (write(1, &c, 1));
+}
