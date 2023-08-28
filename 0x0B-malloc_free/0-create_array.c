7d7eb4219fe974f2dd181ff4677272722511c034
Yvonneuv <ylicenga5@gmail.com>
2023-08-28 14:26:55 -0700

    Task0 float like a butterfly, sting like a bee

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib>
+
+/**
+ * create_array - it creates an array of chars.
+ * @size: size of array
+ * @c: initial value
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		array = (char *)malloc(sizeof(char) * size);
+		if (array == NULL)
+		{
+			return (NULL);
+		}
+		else
+		{
+			for (i = 0; i < size; i++)
+				array[i] = c;
+		}
+	}
+	return (array);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,4 @@
+0x0B-malloc-free
+
+**concepts that are looked into are automatic and dynamic allocation.
+malloc and free.
