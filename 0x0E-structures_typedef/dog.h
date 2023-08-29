9de4fa6a597ae91b15ee3dab60c1d8a89770406b
Yvonneuv <ylicenga5@gmail.com>
2023-08-29 12:00:25 -0700

    Task0 poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,29 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure for dog object
+ * @name: 1st member
+ * @age: 2nd member
+ * @owner: 3rd member
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+* dog_t - Typedef for dog structure
+*/
+typedef struct dog dog_t;
+
+char *_strcpy(char *dest, char *src);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+int _strlen(char *s);
+
+#endif
