c9054ada4db7deae3f8e7e5592946a15553969e0
Yvonneuv <ylicenga5@gmail.com>
2023-08-29 12:39:04 -0700

    Task5

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,10 +14,6 @@ struct dog
 	char *owner;
 };
 
-/**
-* dog_t - Typedef for dog structure
-*/
-typedef struct dog dog_t;
 
 char *_strcpy(char *dest, char *src);
 void init_dog(struct dog *d, char *name, float age, char *owner);
@@ -25,5 +21,10 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 int _strlen(char *s);
+
+/**
+* dog_t - Typedef for dog structure
+*/
+typedef struct dog dog_t;
 
 #endif
